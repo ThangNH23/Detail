e6b4fe89a0fac24cc5657978ada9db145c388c1e
Nguyen Huu Thang <thang@PNVL70.pn.com.vn>
2023-04-15 00:01:24 +0700

    detail

---
+++ detail.php
@@ -0,0 +1,34 @@
+<?php
+$conn = mysqli_connect("localhost","root","","fashion_myshop");
+if (!$conn) {
+    die("Kết nối thất bại: " . mysqli_connect_error());
+  }
+
+  if(isset($_POST['submit'])) {
+    //Sử dụng mệnh đề LIKE ‘%$search_term%’ nó sẽ tìm bất kỳ giá trị nào bằng với $search_term
+    $search_term = $_POST['search_term'];
+    $sql = "SELECT * FROM products WHERE name LIKE '%$search_term%'";
+    $result = mysqli_query($conn, $sql);
+  }
+?>
+
+<!-- Hiển thị biểu mẫu tìm kiếm sản phẩm -->
+<form action="" method="post">
+  <input type="text" name="search_term" placeholder="Tìm kiếm sản phẩm...">
+  <button type="submit" name="submit">Tìm kiếm</button>
+</form>
+
+<!-- Hiển thị kết quả tìm kiếm -->
+<?php if(isset($result)): ?>
+  <?php if(mysqli_num_rows($result) > 0): ?>
+    <ul>
+      <?php while($row = mysqli_fetch_assoc($result)): ?>
+        <li><?php echo $row['name']; ?></li>
+      <?php endwhile; ?>
+    </ul>
+  <?php else: ?>
+    <p>Không tìm thấy sản phẩm nào.</p>
+  <?php endif; ?>
+<?php endif; ?>
+
+<?php mysqli_close($conn); ?>
